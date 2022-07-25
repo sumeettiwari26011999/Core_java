939cdb2c381ed4e7d49cdca8690fd82acbd0c530
sumeettiwari26011999 <109622996+sumeettiwari26011999@users.noreply.github.com>
2022-07-25 23:42:54 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,14 @@
+//Write a java program to find the area of rectangle and square.
+
+class Area
+{
+	public static void main(String args[])
+	{
+		int len=5;
+		int bre=10;
+		int area_of_rectangle=len*bre; //to find area of rectangle
+		int area_of_square=len*len;  //to find area of square
+		System.out.println("Area of Rectangle=" +area_of_rectangle); 
+		System.out.println("Area of Square=" +area_of_square);
+	}
+}
