2516520a7f24fae4a1ff082c025bc8f3e081ce20
sumeettiwari26011999 <109622996+sumeettiwari26011999@users.noreply.github.com>
2022-07-27 09:38:21 +0530

    Create calculations.java

---
+++ calculations.java
@@ -0,0 +1,21 @@
+public class calculations {
+    public static void main(String [] args)
+    {
+        int num1, num2, sum, diff, product;
+        float avg;
+
+        num1 = 7;
+        num2 = 6;
+
+        sum = num1 + num2;
+        diff = num1 - num2;
+        product = num1 * num2;
+        avg = sum/2;
+
+
+        System.out.println("Sum is "+ sum);
+        System.out.println("Difference is "+ diff);
+        System.out.println("Product is "+ product);
+        System.out.println("Average is "+avg);
+    }
+}
