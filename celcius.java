879bfdcd27f664690d4f803cc131a48f1907d77b
sumeettiwari26011999 <109622996+sumeettiwari26011999@users.noreply.github.com>
2022-07-27 09:39:06 +0530

    Create celcius.java

---
+++ celcius.java
@@ -0,0 +1,12 @@
+public class celcius {
+    public static void main(String[] args){
+        float cel, fah;
+        fah=98.6f;
+
+        cel = ((fah-32)*5)/9;
+
+        System.out.println("Entered fahrenheit is "+ fah);
+        System.out.println("Converted celcius is "+ cel);
+    }
+    
+}
